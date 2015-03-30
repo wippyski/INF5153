ac5811574ac9d00506fffbe5cc5ea6aed78e98ea
Steeve Schawann <schawann.steeve_andre_louis@courrier.uqam.ca>
2015-03-30 19:34:26 -0400

    modification ajouterlien

--- editeurcircuit/impl/CircuitImpl.java
+++ editeurcircuit/impl/CircuitImpl.java
@@ -703,7 +703,7 @@ public class CircuitImpl extends MinimalEObjectImpl.Container implements
 				Porte_ORImpl porte_droite_or = (Porte_ORImpl) this
 						.RechercherPorteParNom(p_Droite);
 				porte_droite_or.setEntree1(signal_gauche.getID());
-				if (porte_droite_or.getEntree1() != 0) {
+				if (porte_droite_or.getEntree1() != -1) {
 					porte_droite_or.setEntree2(signal_gauche.getID());
 				} else {
 					porte_droite_or.setEntree1(signal_gauche.getID());
@@ -713,7 +713,7 @@ public class CircuitImpl extends MinimalEObjectImpl.Container implements
 				Porte_AND porte_droite_and = (Porte_AND) this
 						.RechercherPorteParNom(p_Droite);
 				porte_droite_and.setEntree1(signal_gauche.getID());
-				if (porte_droite_and.getEntree1() != 0) {
+				if (porte_droite_and.getEntree1() != -1) {
 					porte_droite_and.setEntree2(signal_gauche.getID());
 				} else {
 					porte_droite_and.setEntree1(signal_gauche.getID());
@@ -740,7 +740,7 @@ public class CircuitImpl extends MinimalEObjectImpl.Container implements
 			case (2):
 				Porte_ORImpl porte_droite_or = (Porte_ORImpl) this
 						.RechercherPorteParNom(p_Droite);
-				if (porte_droite_or.getEntree1() != 0) {
+				if (porte_droite_or.getEntree1() != -1) {
 					porte_droite_or.setEntree2(porte_gauche.getID());
 				} else {
 					porte_droite_or.setEntree1(porte_gauche.getID());
@@ -749,7 +749,7 @@ public class CircuitImpl extends MinimalEObjectImpl.Container implements
 			case (1):
 				Porte_AND porte_droite_and = (Porte_AND) this
 						.RechercherPorteParNom(p_Droite);
-				if (porte_droite_and.getEntree1() != 0) {
+				if (porte_droite_and.getEntree1() != -1) {
 					porte_droite_and.setEntree2(porte_gauche.getID());
 				} else {
 					porte_droite_and.setEntree1(porte_gauche.getID());
