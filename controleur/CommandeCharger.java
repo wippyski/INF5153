0a8214da2e3c51e94f23b60f9ed4f5d202a28e86
Jonathan Milot <jonatansan01@gmail.com>
2015-03-31 12:25:42 -0400

    Correction de bug pour Charger/Sauvegarder. Signed-off-by: Jonathan Milot <jonatansan01@gmail.com>

--- controleur/CommandeCharger.java
+++ controleur/CommandeCharger.java
@@ -1,15 +1,25 @@
 package controleur;
 
+import java.awt.Component;
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 
+
+
+
+
+import javax.swing.JOptionPane;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+
+
 
 
 import editeurcircuit.Circuit;
@@ -30,14 +40,18 @@ public class CommandeCharger implements Commande {
 	
 	public Circuit execute2(File file) {
 		
+		try{
 		  	Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
 			Map<String,Object> m = reg.getExtensionToFactoryMap();
 			m.put("editeurcircuit", new XMIResourceFactoryImpl());
 			ResourceSet resSet = new ResourceSetImpl();
-			URI fileURI = URI.createFileURI(new File(file.getName()).getAbsolutePath());
+			URI fileURI = URI.createFileURI(file.getAbsolutePath());
 			Resource v_circuitResource = resSet.getResource(fileURI,true);
 			circuit = (Circuit)v_circuitResource.getContents().get(0);			
 			circuit.setSauvegarder(true);	
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog( (Component) null,"ERREUR : Nom de fichier invalide.");
+		}
 		
 			return circuit; 
 	}
--- controleur/CommandeSauvegarder.java
+++ controleur/CommandeSauvegarder.java
@@ -1,17 +1,20 @@
 package controleur;
 
 
+import java.awt.Component;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
 
 
 import editeurcircuit.Circuit;
@@ -29,19 +32,19 @@ public class CommandeSauvegarder implements Commande {
 		JFileChooser fc = new JFileChooser();
 		if (fc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
 		  File file = fc.getSelectedFile();	
+		  //System.out.println(file.getName());
 		  try {						
 			Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
 			Map<String,Object> m = reg.getExtensionToFactoryMap();
 			m.put("editeurcircuit", new XMIResourceFactoryImpl());
 			ResourceSet resSet = new ResourceSetImpl();		
-			URI fileURI = URI.createFileURI(new File(file.getName()).getAbsolutePath());		
+			URI fileURI = URI.createFileURI(file.getAbsolutePath());
 			Resource v_circuitResource = resSet.createResource(fileURI);
 			v_circuitResource.getContents().add(circuit);
 			v_circuitResource.save(null);
 			circuit.setSauvegarder(true);
-		  } catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		  } catch (Exception e) {
+			  JOptionPane.showMessageDialog( (Component) null,"ERREUR : Nom de fichier non valide. (Le fichier doit être de forme \"xxx.editeurcircuit\" ");
 		  }
 		}
 		
